46e308da2868b49d75ed96949eddd37f5c4f5cd7
Mrhe05 <291902240@qq.com>
2022-06-09 17:46:32 +0800

    获取当前时间的代码3个调试版本

---
+++ time.c
@@ -0,0 +1,21 @@
+#include<time.h>
+#include<stdio.h>
+#include <unistd.h>
+ int main(void){
+       time_t now ;
+       struct tm *tm_now ;
+       time(&now) ;
+       tm_now = localtime(&now) ;//get date
+  printf("start datetime: %d-%d-%d %d:%d:%d\n",tm_now->tm_year+1900, tm_now->tm_mon+1, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec) ;
+	fflush(stdout);
+       sleep(3);
+
+       time(&now) ;
+
+       tm_now = localtime(&now) ;//get date
+  printf("end datetime: %d-%d-%d %d:%d:%d\n",tm_now->tm_year+1900, tm_now->tm_mon+1, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec) ;
+  //fflush(stdout);
+	sleep(3);
+      return 0;
+
+}
