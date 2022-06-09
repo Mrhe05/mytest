49ff10ae965691ab1b383a27f7d91b95b7e044fe
Mrhe05 <291902240@qq.com>
2022-06-09 11:23:18 +0800

    单引号第一行 单引号第二行 ‘
    ’

--- 1.c
+++ 1.c
@@ -1,13 +1,53 @@
-#include<stdio.h>
+// (VS2019)
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+//局部字体改变颜色
+void color(short x)
+{
+    if (x >= 0 && x <= 15)//参数在0-15的范围颜色
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);	//只有一个参数，改变字体颜色
+    else//默认的颜色白色
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+
+//改变光标位置
+void gotoxy(int x, int y)
+{
+    // 更新光标位置
+    COORD pos;
+    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    pos.X = x;
+    pos.Y = y;
+    SetConsoleCursorPosition(hOutput, pos);
+}
+
+//设置进度条
+void setProgress(int x, int y, int lenth, short procolor, int progress, int max)
+{
+    int pro = (progress * lenth) / max;
+
+    gotoxy(x, y);
+    color(8);
+    for (int i = 0; i < lenth; i++)  //打印进度条背景
+    {
+        printf("█");
+    }
+
+    gotoxy(x, y);
+    color(procolor);
+    for (int i = 0; i < pro; i++)  //打印进度条进度
+    {
+        printf("█");
+    }
+
+    color(16);
+}
 
 int main()
 {
-	printf("打印这句话\n");
-	fflush(stdout);
-	int a;
-	scanf("%d",&a);
-	printf("调试GCC编译器%d",a);
-
+    setProgress(3, 3, 24, 6, 4, 10);
+    gotoxy(4, 4);
 	return 0;
 }
-
--- first.c
+++ first.c
@@ -3,7 +3,7 @@
 int main()
 {
 	int a,b,c;
-	//scanf("%d %d %d",&a,&b,&c);
+	scanf("%d %d %d",&a,&b,&c);
 	printf("请输入你喜欢的三个数字\n");
 	printf("NO1:%d,NO2:%d,NO3:%d\n",a,b,c);
 
