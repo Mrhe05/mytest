4112fe9e6cab2aa7fe9e45877f51de449b9606f1
Mrhe05 <291902240@qq.com>
2022-06-10 15:09:29 +0800

    目的为了理清git restore的作用

---
+++ 1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("努力才会有回报\n");
+	return 0;
+}
--- time.c
+++ time.c
@@ -15,7 +15,7 @@
        tm_now = localtime(&now) ;//get date
   printf("end datetime: %d-%d-%d %d:%d:%d\n",tm_now->tm_year+1900, tm_now->tm_mon+1, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec) ;
   //fflush(stdout);
-	sleep(3);
+	sleep(1);
       return 0;
 
 }
